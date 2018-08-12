0f37dd961d1c54c9c2c2222924aee336ba9f2233
aboud20-meet <aboud20@meet.mit.edu>
2018-08-12 16:20:35 +0300

    Final_project

--- databases.py
+++ databases.py
@@ -1,26 +1,45 @@
 # Database related imports
 # Make sure to import your tables!
-from model import Base, Student
+from model import Base, Student, Tutor, Time
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # You can change the name of your database, just change project.db to whatever you want (make sure to include .db at the end!)
 # Make sure you have the same name for the database in the app.py file!
-engine = create_engine('sqlite:///project.db')
+engine = create_engine('sqlite:///Student.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Your database functions are located under here (querying, adding items, etc.)
 
-# Example of adding a student:
-def add_student(student_name, student_year):
+
+def add_student(student_name, student_location):
     print("Added a student!")
-    student = Student(name=student_name, year=student_year)
+    student = Student(name=student_name, location=student_location)
     session.add(student)
     session.commit()
 
-def get_all_students():
-    students = session.query(Student).all()
-    return students
+def get_tutor_by_username():
+    tutors = session.query(Tutor).all()
+    return tutors
+
+def add_tutor(tutor_username, tutor_name, tutor_password, tutor_location):
+    print("Added a tutor!")
+    tutor = Tutor(username = tutor_username, name = tutor_name, password = tutor_password, location = tutor_location)
+    session.add(tutor)
+    session.commit()
+
+def add_time(time, subject, tutor_username):
+    print("Added Time!")
+    time = Time(time = time, subject = subject, tutor_username = tutor_username)
+    session.add(time)
+    session.commit()
+
+
+# add_tutor("usernme","Mohammad", "passworddd", "Jerusalem")
+
+# add_time("monday", "CS", "usernme")
+
+print(get_tutor_by_username())
--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -10,9 +10,31 @@ Base = declarative_base()
 # Example code:
 class Student(Base):
     __tablename__ = "students"
-    id = Column(Integer, primary_key = True)
+    username = Column(String, primary_key = True)
     name = Column(String)
-    year = Column(Integer)
+    password = Column(String)
+    location = Column(String)
 
     def __repr__(self):
-        return ("Student name: {}, Student year:{}".format(self.name, self.year))
+        return ("Student name: {}, Student's password: {}, Student's Location:{}".format(self.name, self.password, self.location))
+
+class Tutor(Base):
+    __tablename__ = "Tutors"
+    username = Column(String, primary_key = True)
+    name = Column(String)
+    password = Column(String)
+    location = Column(String)
+    times = relationship("Time")
+    def __repr__(self):
+        return ("Tutor Name: {},Tutor's password: {}, Tutor's Location: {}, times: {}".format(self.name,self.password, self.location, self.times))
+
+class Time(Base):
+    __tablename__ = "Time"
+    id = Column(Integer, primary_key = True)
+    time = Column(String)
+    subject = Column(String)
+    # tutor = relationship("Tutor", back_populates="Time")
+    tutor_username = Column(String, ForeignKey("Tutors.username"))
+
+    def __repr__(self):
+        return("time: {},{}")
