0ee342ae29dd5efba2323e6e94f2abc150532660
aboud20-meet <aboud20@meet.mit.edu>
2018-08-13 12:08:00 +0300

    day2

--- app.py
+++ app.py
@@ -1,46 +1,45 @@
 # Flask-related imports
-from flask import Flask, render_template, url_for, redirect, request, session
+from flask import Flask, render_template, url_for, redirect, request, session, flash
 
 # Add functions you need from databases.py to the next line!
-from databases import add_student, get_all_students
+from databases import add_student, get_tutor_by_username, auth_student, get_student_by_username
 
 # Starting the flask app
 app = Flask(__name__)
+app.secret_key = "q34we;kHvWEJWE:KVNl"
 
 # App routing code here
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/tutor/log_in')
-def log_in():
-    return render_template('log_in.html')
-    
-@app.route('/student/log_in')
-def log_in():
-	return render_template('log_in.html')
-
-@app.route('/tutor')
-def tutor():
-    return render_template('tutor_page.html')
-
-@app.route('/student')
-def student():
-    return render_template('student_page.html')
-
-@app.route('/tutor/student_request')
-def student_request():
-    return render_template('student_request_page.html')
-
-@app.route('/student/subjects_page')
-def subjects_page():
-    return render_template('subjects_page.html')
-
-@app.route('/student/subjects_page/tutor_option')
-def tutor_option():
-    return render_template('tutor_option_page.html')
+    if not session.get('logged_in'):
+        return render_template('home.html')
+    else:
+        user = get_student_by_username(session["username"])
+        return render_template("subjects_page.html", student =user )
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def student_login():
+    if request.method == 'POST':
+        POST_USERNAME = str(request.form['username'])
+        POST_PASSWORD = str(request.form['password'])
+        student = auth_student(POST_USERNAME, POST_PASSWORD)
+        
+        if student is not None:
+            session['logged_in'] = True
+            session["username"] = student.username
+            print("logged_in")
+            return redirect(url_for('home'))
+        else:
+            print("wrong")
+            return render_template("log_in.html", error="Bad login")
+    else:
+        return render_template("log_in.html")
+ 
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
 
 # Running the Flask app
 if __name__ == "__main__":
